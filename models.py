1bcc1ce5150ceed77e1d75a946b1d8912a909952
sarahwambui96 <sarahwambui843@gmail.com>
2023-06-09 17:29:06 +0300

    added methods to models.py

--- models.py
+++ models.py
@@ -25,6 +25,15 @@ class Product(Base):
 
     def __repr__(self):
         return f"Product: {self.id} Name: {self.name}"
+    # return the reviews for a product
+    def product_reviews(self):
+        return self.reviews
+    # return the user who reviewed a product
+    def prod_users(self):
+        return self.users
+    # returns an list of all reviews about a product
+    def all_review(self):
+        return[review.full_review() for review in self.reviews]
     
 class User(Base):
     __tablename__ = "users"
@@ -37,15 +46,35 @@ class User(Base):
 
     def __repr__(self):
         return f"User: {self.id} Name: {self.first_name} {self.last_name}"
-    
+    #  return the reviews for a user
+    def user_reviews(self):
+        return self.reviews
+    # return products reviewed by users
+    def user_restaurant(self):
+        return self.products
+    # string format to join first name and last name
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    # return the product with highest rating
+    def favorite_product(self):
+        return max(self.reviews, key=lambda review: review.rating).product
 class Review(Base):
     __tablename__ = "reviews"
 
     id = Column(Integer(), primary_key = True)
     rating = Column(Integer())
-
     product_id = Column(Integer(), ForeignKey("products.id"))
     user_id = Column(Integer(), ForeignKey("users.id"))
+    # return the first user that reviewed
+    def user_review(self):
+        return self.user
+    # return the first product reviewed
+    def product_review(self):
+        return self.product
+    # return full review
+    def full_review(self):
+        return f"Review for {self.product.name} by {self.user.full_name()}: {self.rating} stars"
 
     def __repr__(self):
         return f"Review: {self.id} Rate: {self.rating}"
+    
